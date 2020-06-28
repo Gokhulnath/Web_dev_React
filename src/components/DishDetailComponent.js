1288485d26fdbffcb47d631ec9f7a55887329318
Gokhulnath <gokhulnath18051@cse.ssn.edu.in>
2020-06-29 00:19:28 +0530

    fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardText, CardTitle, CardBody, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control , LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
     function RenderComments({comments, dishId, addComment}){
         if(comments!= null)     
@@ -32,7 +34,7 @@ import { Loading } from './LoadingComponent';
             return(
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
                         <CardBody>
                             <CardTitle><h5>{dish.name}</h5></CardTitle>
                             <CardText>{dish.description}</CardText>
